d928fc22ffab0f12190ec15014099fbdad11e353
tommyHellraiser <nacho.ponce25@gmail.com>
2024-06-12 20:00:16 -0300

    Fixed the fix

--- Forms/Inscription.cs
+++ Forms/Inscription.cs
@@ -95,7 +95,7 @@ namespace SportsClubProject.Forms
             txtLastName.Text = "";
             txtDni.Text = "";
             cboType.Text = "";
-			ckBoxHealth.Enabled = false;
+			ckBoxHealth.Checked = false;
             txtName.Focus();
         }
 
